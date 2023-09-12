36699281ac8e14fb1fc6d7b329701fa7fde8e933
강혁준 <gurwns232@naver.com>
2023-09-12 23:27:37 +0900

    js spa router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import Router from './Router.js';
 import Component from './core/Component.js';
 import CounterPage from './pages/CounterPage.js';
 import FetchPage from './pages/Fetchpage.js';
+import createPage from './pages/index.js';
 
 export default class App extends Component {
     template() {
         return `
+            <header>
+                <a href="#/">Home</a>
+                <a href="#/counter">Counter</a>
+                <a href="#/fetch">Fetch</a>
+            </header>
             <main class="counter-app"></main>
         `;
     }
 
     mounted() {
-        const $counterApp = this.$target.querySelector(`.counter-app`);
-        //new CounterPage($counterApp);
-        new FetchPage($counterApp);
+        const $main = this.$target.querySelector('main');
+        const pages = createPage($main);
+
+        const router = new Router($main);
+        router.addRoute('#/', pages.home);
+        router.addRoute('#/counter', pages.counter);
+        router.addRoute('#/fetch', pages.fetch);
+        router.start();
     }
 }
